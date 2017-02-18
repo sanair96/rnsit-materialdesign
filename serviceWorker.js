03cad23c62ffb7b34237d4c9232092006fb363d7
Sandeep .A <Sandeep .A>
2017-02-18 20:39:02 +0530

    service worker fixed

--- serviceWorker.js
+++ serviceWorker.js
@@ -20,34 +20,6 @@ var initial_cache =['index.html','css/index.css','js/index.js'];
 				})
 		})
 	});
-	self.addEventListener('fetch',event=>{
-		event.respondWith(
-			caches.open(CURRENT_CACHE)
-			.then(cache=>{
-				cache.match(event.request)
-				.then(page=>{
-					if(page){
-						console.log('yolo');
-						return page;}
-					return fetch(event.request)
-					.then(page=>{
-						cache.put(event.request,page.clone());
-						return page;
-					})
-				})
-			})
-)	
-		event.waitUntil(fetch(event.request)
-			.then(page=>{
-				caches.open(CURRENT_CACHE).then(cache=>{
-					cache.put(event.request,page);
-				})
-			})
-			)
-})
-				cache.addAll(initial_cache);
-		})
-	});
 	self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open(CURRENT_CACHE).then(function(cache) {
