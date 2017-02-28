7c18f42e6d7c6cdbe20f1040c646f01eccc88bb7
sanair96 <sandeep_sashok@hotmail.com>
2017-02-28 20:49:50 +0530

    homwpage

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 $( ".logo" ).fadeIn( 600, function() {
+  $(".logo").css("display":"inline");
       $( ".heading" ).fadeIn( 600, function() {
-      $( ".tag" ).fadeIn( 600, function() {});
+        $(".heading").css("display":"inline");
+      $( ".tag" ).fadeIn( 600, function() {
+        $(".tag").css("display":"inline");
+      });
   });
   });
 
